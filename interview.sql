372e2f4d8aec8c7b3f0838c9919aaadf733d6fec
nhathuy007 <55850287+nhathuy007@users.noreply.github.com>
2020-08-25 18:02:57 -0700

    Update SQL

---
+++ interview.sql
@@ -0,0 +1,33 @@
+CREATE TABLE public.groups
+(
+    group_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    group_name character varying COLLATE pg_catalog."default" NOT NULL,
+    group_owner character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT groups_pkey PRIMARY KEY (group_id)
+)
+
+ALTER TABLE public.groups
+    OWNER to postgres;
+
+CREATE TABLE public.group_member
+(
+    group_id integer NOT NULL,
+    email character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT group_member_pkey PRIMARY KEY (group_id, email)
+)
+
+ALTER TABLE public.group_member
+    OWNER to postgres;
+	
+CREATE TABLE public.users
+(
+    email character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (email)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.users
+    OWNER to postgres;
